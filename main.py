2bb0a1fb23767be03724141d36550dcdaa728a0c
Metehan-Yenil <metehanynl@gmail.com>
2024-05-09 22:52:41 +0300

    Revert "Update Yolov8 to DeepFace"
    This reverts commit b9c9745ae53b05b0a673c74b29471aaa6af8439a.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+<<<<<<< b9c9745ae53b05b0a673c74b29471aaa6af8439a
+=======
+<<<<<<< HEAD
+import cv2
+import faceVerified
+
+
+
+
+
+
+
+#-------------------------------------------web cam ile foto alma-----------------------------------------------------------------------------
+def foto_cektir(adi, dizin):
+    # Webcam'i aç
+    kamera = cv2.VideoCapture(0)
+>>>>>>> Revert "Update Yolov8 to DeepFace"
 
 import faceVerified
 
@@ -14,3 +31,10 @@ faceVerified.face_detection(img1_path, img2_path)
 
 
 
+<<<<<<< b9c9745ae53b05b0a673c74b29471aaa6af8439a
+=======
+
+
+=======
+>>>>>>> parent of b9c9745 (Update Yolov8 to DeepFace)
+>>>>>>> Revert "Update Yolov8 to DeepFace"
