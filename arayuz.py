938c381fa5db80ff7d0cfdcdf3ba26e778040e75
Metehan-Yenil <metehanynl@gmail.com>
2024-05-26 12:59:44 +0300

    Face recognition added

--- arayuz.py
+++ arayuz.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 from tkinter import messagebox
 
+def popup_mesaji_goster(message):
+    messagebox.showinfo("Bilgi", message)
 
+def yuz_kayit():
+    popup_mesaji_goster("Yüzünüz başarıyla kaydedildi")
 
-
-
-def popup_mesaji_goster():
-    messagebox.showinfo("Popup Mesajı", "Merhaba! Bu bir pop-up mesajıdır.")
-
+def yuz_dogrulama():
+    popup_mesaji_goster("Yüz doğrulama işlemi başlatıldı")
 
 root = tk.Tk()
 root.title("StayScreenedAndSafe")
 
-# Pencerenin boyutu
 pencere_genislik = 600
 pencere_yukseklik = 400
 ekran_genislik = root.winfo_screenwidth()
@@ -21,9 +21,10 @@ x_pozisyon = (ekran_genislik - pencere_genislik) // 2
 y_pozisyon = (ekran_yukseklik - pencere_yukseklik) // 2
 root.geometry(f"{pencere_genislik}x{pencere_yukseklik}+{x_pozisyon}+{y_pozisyon}")
 
-# Pop-up mesajı gösteren düğmeyi oluştur
-popup_dugme = tk.Button(root, text="Popup Mesajı Göster", command=popup_mesaji_goster)
-popup_dugme.pack(pady=20)
+yuz_kayit_dugme = tk.Button(root, text="Yüz Kayıt", command=yuz_kayit)
+yuz_kayit_dugme.pack(pady=20, padx=10, side=tk.LEFT)
 
-# Uygulamayı başlat
+yuz_dogrulama_dugme = tk.Button(root, text="Yüz Doğrulama", command=yuz_dogrulama)
+yuz_dogrulama_dugme.pack(pady=20, padx=10, side=tk.LEFT)
+
 root.mainloop()
